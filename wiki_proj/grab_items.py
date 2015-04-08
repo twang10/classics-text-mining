214a3c2a07408a8287ea9f101e9cc5a3078a430c
Jason Fan <jason.fan.74@gmail.com>
2015-04-08 15:20:45 -0400

    last minute changes before presenting

--- wiki_proj/grab_items.py
+++ wiki_proj/grab_items.py
@@ -4,17 +4,18 @@ infoboxes = []
 
 with open("examples_latd+infobox.xml") as f:
 	box_s = ''
-	state = False
+	in_itembox = False
+	has_locs = False
 	opens = 0
 	closes = 0
 	for line in f:
 		if (state == False):
-			if ("{{Infobox"  in line):
+			if ("{{Infobox"  in line ): #(and "settlement" in line):
 				opens += 1
 				attr = line[10:]
 				attr = attr.rstrip()
 				box_s += '<infobox ' + 'class="'+attr +'">\n' + line
-				state = True
+				in_itembox = True
 				continue
 
 		#START COLLECTING
@@ -22,12 +23,16 @@ with open("examples_latd+infobox.xml") as f:
 			opens += line.count("{{")
 			closes += line.count("}}")
 			box_s += line
+			if('latd' in line):
+				has_locs = True
 			#print(opens, closes)
 
 			#TODO: -look for latd and longd in line
 			if (opens == closes):
-				infoboxes.append(box_s + '</infobox>')
-				state = False
+				if has_locs:
+					infoboxes.append(box_s + '</infobox>')
+				has_locs = False
+				in_itembox = False
 
 for line in infoboxes:
 	print(line)
